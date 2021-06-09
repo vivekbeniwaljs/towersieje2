1186c482e385bfd45a520d524a1640cbdbf768d0
vivekbeniwaljs <76515013+vivekbeniwaljs@users.noreply.github.com>
2021-06-09 22:44:36 +0530

    Add files via upload

---
+++ Block2.js
@@ -0,0 +1,36 @@
+class Block2{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        
+      }
+      display(){
+        
+         var pos =this.body.position;
+        
+         
+        
+        push();     
+        rectMode(CENTER);
+        fill(rgb(240,128,128));
+        rect(pos.x, pos.y, this.width, this.height);
+        
+        pop();
+
+
+
+        
+        
+        
+        
+        
+       
+      }
+  }
